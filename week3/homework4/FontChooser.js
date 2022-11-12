8dde7147b0b981dd85b9de89494abc48f3378ebe
Marcio Bueno <mabuen@live.com>
2022-11-12 19:07:13 +0100

    homework 4 2

--- week3/homework4/FontChooser.js
+++ week3/homework4/FontChooser.js
@@ -2,18 +2,26 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
+	this.state={bold:"normal", size:16, hidden:"true"}
     }
+
+	handleClick(){
+		this.setState({hidden:(this.state.hidden?"false":"true")})
+	}
     
 
     render() {
+		var bold=this.state.bold;
+		var size=this.state.size;
+		var hidden=this.state.hidden;
 
 	return(
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
+	       <input type="checkbox" id="boldCheckbox" hidden={{hidden}}/>
 	       <button id="decreaseButton" hidden='true'>-</button>
 	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
 	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
+	       <span id="textSpan" onClick={this.handleClick.bind(this)}>{this.props.text}</span>
 	       </div>
 	);
     }
