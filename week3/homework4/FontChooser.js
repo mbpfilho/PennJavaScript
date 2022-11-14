e9add54500cbfb2905b205c160c4422882ce9c02
Marcio Bueno <mabuen@live.com>
2022-11-14 10:40:51 +0100

    homework4 hidden

--- week3/homework4/FontChooser.js
+++ week3/homework4/FontChooser.js
@@ -2,26 +2,28 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state={bold:"normal", size:16, hidden:"true"}
+	this.state={bold:"normal", size:16, hidden:true}
     }
 
-	handleClick(){
-		this.setState({hidden:(this.state.hidden?"false":"true")})
+	toggle(){
+		this.setState({hidden: !this.state.hidden})
 	}
     
 
     render() {
 		var bold=this.state.bold;
 		var size=this.state.size;
-		var hidden=this.state.hidden;
+		var hidden=this.state.hidden?"none":"inline";
 
 	return(
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden={{hidden}}/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan" onClick={this.handleClick.bind(this)}>{this.props.text}</span>
+			<div style={{display: hidden}}>
+				<input type="checkbox" id="boldCheckbox" />
+				<button id="decreaseButton">-</button>
+				<span id="fontSizeSpan">{this.props.size}</span>
+	       <button id="increaseButton">+</button>
+			</div>
+	       <span id="textSpan" onClick={this.toggle.bind(this)}>{this.props.text}</span>
 	       </div>
 	);
     }
