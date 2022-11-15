ded741b09522397ac793a50e8032a8ee663c75a6
Marcio Bueno <mabuen@live.com>
2022-11-15 10:11:02 +0100

    homework 4 final

--- week3/homework4/FontChooser.js
+++ week3/homework4/FontChooser.js
@@ -2,9 +2,13 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state={bold:this.props.bold=='true'?true:false,
+	
+	this.state={bold:this.props.bold=='true'?"bold":"normal",
          size:Number(this.props.size)<Number(this.props.min)?Number(this.props.min):Number(this.props.size)>Number(this.props.max)?Number(this.props.max):Number(this.props.size), 
-         hidden:true}
+        hidden:true,
+		min:Number(this.props.min)>0?Number(this.props.min):1,
+		max:Number(this.props.max)<Number(this.props.min)?Number(this.props.min):Number(this.props.max),
+		check:this.props.bold=='true'?true:false}
     }
 
 	toggleHidden(){
@@ -12,33 +16,32 @@ class FontChooser extends React.Component {
 	}
 
 	toggleBold(){
-		this.setState({bold:!this.state.bold})
+		this.setState({bold:this.state.bold=="bold"?"normal":"bold"});
+		this.setState({check:!this.state.check})
 	}
     
 	increaseSize(){
-		this.setState({size:this.state.size<this.props.max?this.state.size+1:max})
+		this.setState({size:this.state.size<this.props.max?this.state.size+1:this.state.max})
 	}
 	decreaseSize(){
-		this.setState({size:this.state.size>this.props.min?this.state.size-1:min})
+		this.setState({size:this.state.size>this.props.min?this.state.size-1:this.state.min})
 	}
 	returnSize(){
 		this.setState({size:Number(this.props.size)})
 	}
 
     render() {
-		var bold=this.state.bold?"bold":"normal";
-		var check=this.state.bold?"checked":"";
+		var bold=this.state.bold;
+		var check=this.state.check;
 		var size=this.state.size;
 		var sizecolor="black";
         var hide=this.state.hidden;
 		if(size==this.props.max||size==this.props.min)sizecolor="red";
-		var min=Number(this.props.min)>0?Number(this.props.min):1;
-		var max=Number(this.props.max)<Number(this.props.min)?Number(this.props.min):Number(this.props.max);
 
 
 	return(
 	       <div>
-				<input type="checkbox" id="boldCheckbox" hidden={hide} checked={check} onClick={this.toggleBold.bind(this)}/>
+				<input type="checkbox" id="boldCheckbox" hidden={hide} checked={check} onChange={this.toggleBold.bind(this)}/>
 				<button id="decreaseButton" hidden={hide} onClick={this.decreaseSize.bind(this)}>-</button>
 				<span id="fontSizeSpan" style={{color:sizecolor}} hidden={hide} onDoubleClick={this.returnSize.bind(this)}>{size}</span>
 	       		<button id="increaseButton" hidden={hide} onClick={this.increaseSize.bind(this)}>+</button>
