9971acf749bff74642d7c57a6851320d8e05338b
Marcio Bueno <mabuen@live.com>
2022-11-24 12:42:27 +0100

    express mongoose

---
+++ week4/bookApp/routes/Book.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+// where does the db name come from? I just made this up
+// mongoose.connect('mongodb://localhost:27017/myDatabase');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user:admin@cluster0.22kghxc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+var Schema = mongoose.Schema;
+
+var authorSchema = new Schema({
+	name: String,
+	affiliation: String
+    });
+
+var bookSchema = new Schema({
+	title: {type: String, required: true, unique: true},
+	year: Number,
+	authors: [authorSchema]
+    });
+
+
+module.exports = mongoose.model('Book', bookSchema);
+
