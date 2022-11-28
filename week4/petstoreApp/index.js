fbb86632e89606b56dd934f44558f2f323f80b23
Marcio Bueno <mabuen@live.com>
2022-11-29 00:37:34 +0100

    homework 6 3

--- week4/petstoreApp/index.js
+++ week4/petstoreApp/index.js
@@ -6,23 +6,60 @@ var Toy = require('./Toy.js');
 
 //---------------------------------------------
 
-app.use('/findToy', (req, res) => {
-	
-	
-	if (req.query.id) var id = req.query.id;
+app.use('/findToy',(req, res)=>{	
+	if(req.query.id)var id=req.query.id;
     else res.json({});
 
-	Toy.findOne({id:id}, (err, toy) => {
-		if (err) {
+	Toy.find({id:id},(err,toy)=>{
+		if(err){
 		    res.type('html').status(500);
-		    res.send('Error: ' + err);
-        }else if (!toy) res.json({});
+		    res.send('Error: '+err);
+        }else if(!toy)res.json({});
         else res.json(toy);
-	    });
-	
+	});
 })
 
+app.use('/findAnimals',(req,res)=>{
+    if(!req.query.species&&!req.body.gender&&!req.query.trait)res.json({});
+    else{
+        var terms=[];
+        if(req.query.species)terms.push({species:req.query.species});
+        if(req.body.gender)terms.push({gender:req.query.gender});
+        if(req.query.trait)var trait={traits:req.query.trait};
+    
+        var query={$or:[terms,trait]};
 
+	    Animal.find(query,(err,animals)=>{
+		    if(err){
+		        res.type('html').status(500);
+		        res.send('Error: '+err);
+		    }else if(!animals)res.json({});
+	        else{ 
+                fanimals=[];
+                animals.forEach((animal)=>{fanimals.push({name:animal.name,species:animal.species,breed:animal.breed,gender:animal.gender,age:animal.age})});
+                res.json({fanimals});
+
+            }
+        })
+    }
+})
+
+app.use('/animalsYoungerThan',(req,res)=>{	
+	if(req.query.age)var age=req.query.age;
+    else res.json({});
+
+	Animal.find({age:{$lt:age}},(err,animals)=>{
+		if(err){
+		    res.type('html').status(500);
+		    res.send('Error: '+err);
+        }else if(!animals)res.json({count:0})
+        else{
+            names=[];
+            animals.forEach((animal)=>{names.push(animal.name)});
+            res.json({count:animals.length,names:names});
+        }
+	});
+})
 
 
 //---------------------------------------------
