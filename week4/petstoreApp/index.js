2abc41f98a036e4859388b202fa2964a4de630bd
Marcio Bueno <mabuen@live.com>
2022-11-29 11:56:32 +0100

    homework 6 14/14

--- week4/petstoreApp/index.js
+++ week4/petstoreApp/index.js
@@ -25,40 +25,75 @@ app.use('/findAnimals',(req,res)=>{
         var terms=[];
         if(req.query.species)terms.push({species:req.query.species});
         if(req.body.gender)terms.push({gender:req.query.gender});
-        if(req.query.trait)var trait={traits:req.query.trait};
+        if(req.query.trait)terms.push({traits:req.query.trait});
+        // if(req.query.trait)var trait={traits:req.query.trait};
     
-        var query={$or:[terms,trait]};
+        // var query={$or:terms};
 
-	    Animal.find(query,(err,animals)=>{
+	    Animal.find(terms,(err,animals)=>{
 		    if(err){
 		        res.type('html').status(500);
 		        res.send('Error: '+err);
-		    }else if(!animals)res.json({});
+		    }else if(animals.length==0)res.json({});
 	        else{ 
-                fanimals=[];
-                animals.forEach((animal)=>{fanimals.push({name:animal.name,species:animal.species,breed:animal.breed,gender:animal.gender,age:animal.age})});
-                res.json({fanimals});
-
+                var foundAnimals=[];
+                animals.forEach((animal)=>{foundAnimals.push({name:animal.name,species:animal.species,breed:animal.breed,gender:animal.gender,age:animal.age})});
+                res.json(foundAnimals);
             }
         })
     }
 })
 
 app.use('/animalsYoungerThan',(req,res)=>{	
-	if(req.query.age)var age=req.query.age;
-    else res.json({});
+	if(!req.query.age)res.json({});
+    else{
+        var age=req.query.age;
+        Animal.find({age:{$lt:age}},(err,animals)=>{
+            if(err){
+                res.type('html').status(500);
+                res.send('Error: '+err);
+            }else if(animals.length==0)res.json({count:0})
+            else{
+                var names=[];
+                animals.forEach((animal)=>{names.push(animal.name)});
+                res.json({count:animals.length,names:names});
+            }
+        });
+    }
+})
 
-	Animal.find({age:{$lt:age}},(err,animals)=>{
-		if(err){
-		    res.type('html').status(500);
-		    res.send('Error: '+err);
-        }else if(!animals)res.json({count:0})
-        else{
-            names=[];
-            animals.forEach((animal)=>{names.push(animal.name)});
-            res.json({count:animals.length,names:names});
+app.use('/calculatePrice',(req,res)=>{
+    var idlength=req.query.id.length;
+	if(idlength>0&&idlength==req.query.qty.length){
+        var id=[];
+        var qty=[];
+        for(let i=0;i<idlength;i++){
+            id[i]=req.query,id[i];
+            qty[i]=req.query.id[i];
         }
-	});
+        Toy.find({id:id},(err,toys)=>{
+            if(err){
+                res.type('html').status(500);
+                res.send('Error: '+err);
+            }else if(!toys)res.json({})
+            else{
+                var toreturn={};
+                var totalPrice=0;
+                var items=[];
+                var subtotal;
+                var price;
+
+                for(let c=0;c<idlength;c++){
+                    subtotal=
+                }
+                                
+                .forEach((animal)=>{names.push(animal.name)});
+                res.json({count:animals.length,names:names});
+            }
+        });
+
+    }else res.json({});
+
 })
 
 
